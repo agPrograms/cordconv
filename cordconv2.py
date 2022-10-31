38b2718b87a1f8e818bfb51c108be3b9ffb9fae6
agPrograms <107636785+agPrograms@users.noreply.github.com>
2022-10-31 18:27:35 -0400

    Added Feature: Copy results to .txt fille
    + Users now have the option to copy the results to a text file (coords123.txt). Which will automatically be opened upon creation!
    - Fixed bug at line 5, must be str or min cannot be added to sec.
    - You can now do Lat(N)/Lon(E) in one go. So now users do not have to go back to the menu.
    - Created coords list so outputs of each dms2ddm call can be appended as North and East separately.

--- cordconv2.py
+++ cordconv2.py
@@ -1,16 +1,18 @@
 import subprocess
+import re
+import os
 
+file = 'coords123.txt'
 # Convert Degreess Minutes Seconds to Degrees Decimal Minutes
 def dms2ddm(deg,min,sec):
-    sec1 = float(str(sec/60)[0:5])+min # improved since 1.0. 
-    ddm2 = int(deg), sec1 # improved since 1.0 
+    sec1 = str(float(str(sec/60)[0:5])+min) # improved since 1.0.
+    ddm2 = int(deg), sec1 # improved since 1.0
     return ddm2
 
 # Copy output to clipboard
 def copy2clip(txt):
     cmd='echo '+txt.strip()+'|clip'
     return subprocess.check_call(cmd, shell=True)
-
 menu = {}
 menu['1']="Convert DMS to Degress Decimal Minutes"
 menu['2']="Exit"
@@ -20,18 +22,46 @@ while True:
         print(entry, menu[entry])
     selection=input("Please Select: ") 
     if selection =='1': 
+        coords = []
+        # Get the north coordinates from the user
         print("Convert DMS to Degrees Decimal Minutes \n")
-        print("Enter Degrees \n")
+        print("Enter North Degrees \n")
         deg = float(input())
-        print("Enter Minutes \n")
+        print("Enter North Minutes \n")
         min = float(input())
-        print("Enter Seconds \n")
+        print("Enter North Seconds \n")
         sec = float(input())
         ddmResult = dms2ddm(deg,min,sec)
-        print('\nDDM: ', ddmResult, '\n')
-        #print(dms2ddm(deg,min,sec))
+        print('\nDDM: N', ddmResult, '\n')
         copy2clip(str(ddmResult))
-        print('Degrees Decimal Minutes copied to clipboard! \n')
+        coords.append(str('N'+str(ddmResult)))
+        print('NORTH Degrees Decimal Minutes copied to clipboard! \n')
+
+        # Get the east coordinates from the user
+        print("Enter East Degrees \n")
+        deg = float(input())
+        print("Enter East Minutes \n")
+        min = float(input())
+        print("Enter East Seconds \n")
+        sec = float(input())
+        ddmResult = dms2ddm(deg,min,sec)
+        print('\nDDM: E', ddmResult, '\n')
+        copy2clip(str(ddmResult))
+        coords.append(str('E'+str(ddmResult)))
+        print('EAST Degrees Decimal Minutes copied to clipboard! \n')
+        
+        # Ask the user if they want to copy the coordinates to a notepad document.
+        print('Copy both coordinates to a txt file/notepad? [Y/n]\n');print('!!!*** WARNING: This will overwrite the file coords123.txt where this is executed! ***!!!\n')
+        fnlchoice = str(input('Y/n: ').lower()) # convert user input to lower case
+        if fnlchoice.startswith('y'):
+            data = re.sub('[()''"",]', '', str(coords)[1:-1]) # GET RID OF SYMBOLS AND []
+            with open(file, 'w') as f:
+                for item in data:
+                    f.write("%s" % item) # I cant use \n here because it will add a new line for every character. THANKS REGEX...
+            subprocess.Popen(['notepad.exe', file])
+            print('\nCopied to file: ',os.getcwd()+file, '\n')
+        elif fnlchoice.startswith('n'):
+            print('Ok! Not copying to notepad. \n')
     elif selection == '2': 
         break
     else: 
